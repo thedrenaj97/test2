6ea362b7318073700650d0841a23779efbee7316
Andrej Markovic <thedrenaj@gmail.com>
2020-07-30 12:07:35 +0200

    add input from csv file

--- Tutorials_and_Examples/src/Main.java
+++ Tutorials_and_Examples/src/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		System.out.println("Hellow World");
+		//int x = 0;
+		Scanner in = new Scanner(new File("src\\data.csv"));
+		
+		List<Double> names = new ArrayList<Double>();
+		in.useDelimiter(",");
+		
+		while(in.hasNext()) {
+			names.add(Double.parseDouble(in.nextLine()));	
+		}
+		for(int i=0; i<names.size();i++) {
+			System.out.println("Sample number:      " +(i+1) + ";                  Sample value: " + names.get(i));
+		}
+		in.close();
+		
 	}
 
 }
+ 
